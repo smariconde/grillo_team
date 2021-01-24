4fc90ad345cec3f113f855e4c184741d63d19e39
smariconde <smariconde@gmail.com>
2021-01-24 19:36:30 -0300

    Added created_at

--- finanzas_sql.py
+++ finanzas_sql.py
@@ -4,7 +4,7 @@ def getEarningsDate(symbol):
     try:
         sql_engine = create_engine('mysql+pymysql://root:@34.66.111.107/finanzas')
         sql_conn = sql_engine.connect()
-        q = f"SELECT earnings FROM finviz WHERE symbol = '{symbol}'"
+        q = f"SELECT earnings FROM finviz WHERE symbol = '{symbol}' ORDER BY created_at DESC"
         earnings = sql_conn.execute(q).fetchone()
         sql_conn.close()
     except:
